ba9329e2bc5071a54b929eaaaacea51d7ad7d30e
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-06 15:09:38 -0300

    feat: métodos CRUD

---
+++ src/main/java/com/portfolio/api_users/business/UserService.java
@@ -0,0 +1,43 @@
+package com.portfolio.api_users.business;
+
+import com.portfolio.api_users.infrastructure.entity.User;
+import com.portfolio.api_users.infrastructure.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void saveUser (User user){
+        repository.save(user);
+    }
+
+    public User findUserById (Long id){
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+    }
+
+    public void updateUser (Long id, User user){
+        User userEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado.")
+        );
+
+        User updatedUser = User.builder()
+                .id(userEntity.getId())
+                .name(user.getName() != null ? user.getName() : userEntity.getName())
+                .email(user.getEmail() != null ? user.getEmail() : userEntity.getEmail())
+                .password(user.getPassword() != null ? user.getPassword() : userEntity.getPassword())
+                .dateBirth(user.getDateBirth() != null ? user.getDateBirth() : userEntity.getDateBirth())
+                .build();
+        repository.save(updatedUser);
+    }
+
+    public void deleteUser (Long id){
+        repository.deleteById(id);
+    }
+}
