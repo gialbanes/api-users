f4fc48f8e2fab52c963f8fdb754a14a4361b8ccf
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-12 13:10:46 -0300

    feat: requisições HTTP do product

---
+++ src/main/java/com/portfolio/api_users/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.portfolio.api_users.controller;
+
+import com.portfolio.api_users.business.ProductService;
+import com.portfolio.api_users.infrastructure.entity.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createProduct (@RequestBody Product product){
+         productService.addProduct(product);
+         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Product> getProduct (@RequestParam Long id){
+        productService.findProductById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateProduct (@RequestParam Long id, @RequestBody Product product){
+        productService.updateProductById(id, product);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct (@RequestParam Long id){
+        productService.deleteProductById(id);
+        return ResponseEntity.ok().build();
+    }
+}
