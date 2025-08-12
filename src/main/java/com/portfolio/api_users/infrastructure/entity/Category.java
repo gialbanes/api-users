6e73eb8409d586444b95b4ae937421180e775da1
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-12 13:08:53 -0300

    feat: novas entitys

---
+++ src/main/java/com/portfolio/api_users/infrastructure/entity/Category.java
@@ -0,0 +1,26 @@
+package com.portfolio.api_users.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.portfolio.api_users.infrastructure.entity.Product;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
---
+++ src/main/java/com/portfolio/api_users/infrastructure/entity/Rating.java
@@ -0,0 +1,31 @@
+package com.portfolio.api_users.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "rating")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer grade;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
