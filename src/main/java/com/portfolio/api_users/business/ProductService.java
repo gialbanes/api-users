064954c1dd4419a1829b20199235af585d05b55a
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-12 13:10:25 -0300

    feat: métodos CRUD do product

---
+++ src/main/java/com/portfolio/api_users/business/ProductService.java
@@ -0,0 +1,45 @@
+package com.portfolio.api_users.business;
+
+import com.portfolio.api_users.infrastructure.entity.Product;
+import com.portfolio.api_users.infrastructure.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private final ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addProduct(Product product){
+        repository.save(product);
+    }
+
+    public Product findProductById(Long id){
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Produto não encontrado.")
+        );
+    }
+
+    public void updateProductById(Long id, Product product){
+        Product productEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Produto não encontrado.")
+        );
+
+        Product updatedProduct = Product.builder()
+                .id(productEntity.getId())
+                .name(product.getName() != null ? product.getName() : productEntity.getName())
+                .description(product.getDescription() != null ? product.getDescription() : productEntity.getDescription())
+                .price(product.getPrice() != null ? product.getPrice() : productEntity.getPrice())
+                .stock(product.getStock() != null ? product.getStock() : productEntity.getStock())
+                .category(product.getCategory() != null ? product.getCategory() : productEntity.getCategory())
+
+                .build();
+        repository.save(updatedProduct);
+    }
+
+    public void deleteProductById(Long id){
+        repository.deleteById(id);
+    }
+}
