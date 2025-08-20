2079c87a29bff08952c28526ac8c73d42a6daf0b
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-20 17:45:27 -0300

    feat: método para buscar usuário por email

--- src/main/java/com/portfolio/api_users/infrastructure/repository/UserRepository.java
+++ src/main/java/com/portfolio/api_users/infrastructure/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.portfolio.api_users.infrastructure.repository;
 
-import com.portfolio.api_users.infrastructure.entity.User;
+import com.portfolio.api_users.infrastructure.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
 }
