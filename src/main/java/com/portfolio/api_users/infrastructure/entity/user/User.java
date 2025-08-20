de454ab2764f19ec11716041fcec331a3c09db4d
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-20 17:43:18 -0300

    refactor: add atributo role

--- src/main/java/com/portfolio/api_users/infrastructure/entity/user/User.java
+++ src/main/java/com/portfolio/api_users/infrastructure/entity/user/User.java
@@ -1,11 +1,11 @@
-package com.portfolio.api_users.infrastructure.entity;
+package com.portfolio.api_users.infrastructure.entity.user;
 
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -32,11 +32,20 @@ public class User implements UserDetails {
     private String password;
 
     @Column(nullable = false)
-    private String role;
+    private UserRole role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        if(this.role == UserRole.ADMIN){
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        }
     }
 
     @Override
