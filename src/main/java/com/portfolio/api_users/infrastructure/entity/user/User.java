fbabf6c84062406f41ab2c6203b321dffcb754ca
Giovana Albanês <albanesgiovana@gmail.com>
2025-08-20 17:38:30 -0300

    feat: permitir que o spring security acesse os usuários

--- src/main/java/com/portfolio/api_users/infrastructure/entity/user/User.java
+++ src/main/java/com/portfolio/api_users/infrastructure/entity/user/User.java
@@ -2,8 +2,12 @@ package com.portfolio.api_users.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name="users")
@@ -13,7 +17,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +30,17 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
