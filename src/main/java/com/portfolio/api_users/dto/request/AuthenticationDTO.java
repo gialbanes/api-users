bf79a428f3e621dd38e2bcae489cbcc106208960
DSM4 <fatec-dsm4@academico.intra>
2025-08-21 20:39:52 -0300

    feat: dto da rota de login do usuário

---
+++ src/main/java/com/portfolio/api_users/dto/request/AuthenticationDTO.java
@@ -0,0 +1,31 @@
+package com.portfolio.api_users.dto.request;
+
+public class AuthenticationDTO {
+
+    private String email;
+    private String password;
+
+    // Construtor
+    public AuthenticationDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
