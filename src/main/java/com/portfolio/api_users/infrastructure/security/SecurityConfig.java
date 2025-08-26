1dfe0fdef7a593577de072ca3bc9f20e78a6ad74
DSM4 <fatec-dsm4@academico.intra>
2025-08-25 22:00:42 -0300

    feat: configuração de autenticação e início do hash de senha

--- src/main/java/com/portfolio/api_users/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/portfolio/api_users/infrastructure/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.portfolio.api_users.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +25,16 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             ) 
             .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(authenticationConfiguration authenConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    // configuração de hash de senha
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
