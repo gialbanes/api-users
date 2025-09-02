bf76dbfff238ecba81da3a5c4de368bca8e140f6
Giovana Albanês <albanesgiovana@gmail.com>
2025-09-02 15:59:32 -0300

    docs: funcionamento do filtro que libera as requisições

--- src/main/java/com/portfolio/api_users/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/portfolio/api_users/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.portfolio.api_users.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,33 +12,47 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; // Importação correta
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private final SecurityFilter securityFilter;
+
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
     @Bean
+    // a corrente de filtros que eu vou aplicar pra fazer a segurança da minha aplicação; métodos que vão validar o usuário na requisição
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
-                    .anyRequest().authenticated()
-            ) 
-            .addFilterBefore(securityFilter, UsernamePassowrdAuthenticationFilter.class)
-            .build();
+                // tira a tela de login padrão do Spring Security e abre as rotas
+                .csrf(csrf -> csrf.disable())
+                // autenticação stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // quais são as requisições HTTP que eu vou autorizar
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                        // só preciso que esteja autenticado independente da role; para estar autenticado, precisa de um login validado
+                        .anyRequest().authenticated()
+                )
+                // filtro que acontece antes de verificar as requisições do filtro anterior
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
-
     }
+
     @Bean
-    // configuração de hash de senha
+    // classe BCryptPasswordEncoder do spring security para criptografar a senha no banco de dados
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
