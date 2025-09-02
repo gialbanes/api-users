969652c680eced2cb01f07604aa3c5241666cecb
Giovana Albanês <albanesgiovana@gmail.com>
2025-09-02 16:01:07 -0300

    refactor: transforma de class em record

--- src/main/java/com/portfolio/api_users/dto/request/AuthenticationDTO.java
+++ src/main/java/com/portfolio/api_users/dto/request/AuthenticationDTO.java
@@ -1,28 +1,5 @@
 package com.portfolio.api_users.dto.request;
 
-public class AuthenticationDTO {
+public record AuthenticationDTO (String email, String password) {
 
-    private String email;
-    private String password;
-
-    public AuthenticationDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
