f6e3cd142fca1028645acc45a0acb0b14c041ea6
Giovana Albanês <albanesgiovana@gmail.com>
2025-09-04 17:44:45 -0300

    fix: corrige de ROLE_USER para ROLE_CUSTOMER

--- src/main/java/com/portfolio/api_users/entity/user/User.java
+++ src/main/java/com/portfolio/api_users/entity/user/User.java
@@ -55,11 +55,11 @@ public class User implements UserDetails {
         if(this.role == UserRole.ADMIN){
             return List.of(
                     new SimpleGrantedAuthority("ROLE_ADMIN"),
-                    new SimpleGrantedAuthority("ROLE_USER")
+                    new SimpleGrantedAuthority("ROLE_CUSTOMER")
             );
         } else {
             return List.of(
-                    new SimpleGrantedAuthority("ROLE_USER")
+                    new SimpleGrantedAuthority("ROLE_CUSTOMER")
             );
         }
     }
